7864626e0c18ced328734f8a70ebacdca712526d
ryangjchandler <ryangjchandler@users.noreply.github.com>
2023-11-13 12:01:47 +0000

    Fix styling

--- src/PasswordlessLoginServiceProvider.php
+++ src/PasswordlessLoginServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use RyanChandler\PasswordlessLogin\Commands\PasswordlessLoginCommand;
+use RyanChandler\PasswordlessLogin\Testing\TestsPasswordlessLogin;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use RyanChandler\PasswordlessLogin\Commands\PasswordlessLoginCommand;
-use RyanChandler\PasswordlessLogin\Testing\TestsPasswordlessLogin;
 
 class PasswordlessLoginServiceProvider extends PackageServiceProvider
 {
